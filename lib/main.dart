d92a4d2717e16108ecb02799ef6e4fe9ff9874ab
MdNurujjaman Shipat <mmdshipat100@gmail.com>
2026-05-13 19:49:22 +0600

    bkash app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:bkash_app/core/app_colors.dart';
+import 'package:bkash_app/presentation/provider/home_provider.dart';
+import 'package:bkash_app/presentation/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => HomeProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
